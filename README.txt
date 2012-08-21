a8d06a4f3f3b5017c2c202b08b5a5b4529c41ab4
codepeople <codepeople@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-08-21 13:57:11 +0000

    git-svn-id: https://plugins.svn.wordpress.org/codepeople-theme-switch/trunk@588334 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ README.txt
@@ -0,0 +1,58 @@
+=== CodePeople Theme Switch ===
+Contributors: codepeople
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=GZX2Y9BBJKWN2
+Tags: switch, theme, mobile, codepeople
+Requires at least: 3.0.5
+Tested up to: 3.3.2
+Stable tag: 1.0.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+**CodePeople-Theme-Switch** allows to swap the active theme to the CodePeople-Light theme when your WordPress website is loaded on a mobile device. With **CodePeople-Theme-Switch** plugin, you can configure main features of the CodePeople-Light theme without having to activate it.
+
+== Description ==
+
+**CodePeople-Theme-Switch** checks the "user agent" of the browser that make the server's request and identifies the device used for browsing. If a mobile device is detected (phone, table, ebook reader or games console) the **CodePeople-Theme-Switch** plugin loads the **CodePeople-Light theme** (**CodePeople-Light theme** is a WordPress theme based on jQueryMobile developed specifically for mobile devices).
+
+**The switch is made automatically**: no action is needed from the WordPress administrator. If the default design of CodePeople-Light theme doesn't meet your expectations, **you can modify the theme colors through the CodePeople-Theme-Switch plugin**.
+
+
+**Features:**
+
+Main features of **CodePeople-Theme-Switch** are:
+*  Automatic switching of theme if the website is loaded on mobile browsers.
+*  Allows to configure the CodePeople-Light theme's color scheme.
+
+If you want more information about this plugin, please visit [http://wordpress.dwbooster.com](http://wordpress.dwbooster.com "CodePeople WordPress Repository"), where you can also check out other WordPress plugins. 
+
+
+== Installation ==
+
+**To install CodePeople-Theme-Switch, follow these steps:**
+
+1.	Download and unzip the plugin
+2.	Upload the entire *codepeople-theme-switch/* directory to the */wp-content/plugins/* directory 
+3.	Activate the plugin through the Plugins menu in WordPress
+4.	Go to the plugin's settings section if you want to modify the color scheme in CodePeople-Light theme
+
+**Requirements:**
+
+For the CodePeople-Theme-Switch plugin to function properly,  [http://wordpress.dwbooster.com/themes/codepeople-light](http://wordpress.dwbooster.com/themes/codepeople-light "CodePeople-Light Theme") needs to be uploaded to the */wp-content/themes* directory.
+
+Notice: If you have configured WordPress as a **multisite WordPress**, then the CodePeople-Light theme must be enabled for the Network to be accessible by the plugin.
+
+== Frequently Asked Questions ==
+
+= Q: Why website is displayed as a blank page in mobiles devices? =
+A: Usually a blank page is displayed in mobiles devices when the CodePeople-Light theme is not installed in the WordPress, or the theme is disabled by the network if the WordPress is used in "multisite" mode.
+
+= Q: Why does my website continue to display the active theme instead of CodePeople-Light when the website is loaded on a mobile device? =
+A: If the WordPress loads the active theme and not the CodePeople-Light theme, it is likely that your device is not identified as a mobile device. In this case please contact us and send us the brand and model of your device and the browsers used in it.
+
+For more information, please visit our FAQ page in:
+http://wordpress.dwbooster.com/faq/codepeople-theme-switch
+
+== Screenshots ==
+
+1. Configuration settings.
+2. Print screen of CodePeople-Light theme used in an iPhone.
---
+++ codepeople-theme-switch.php
@@ -0,0 +1,206 @@
+<?php 
+/*  
+Plugin Name: CodePeople Theme Switch
+Plugin URI: http://wordpress.dwbooster.com/content-tools/codepeople-theme-switch
+Version: 1.0.1
+Author: <a href="http://www.codepeople.net">CodePeople</a>
+Description: "CodePeople Theme Switch" loads the CodePeople Light theme if the website is loaded from a mobile browser. From "CodePeople Theme Switch" is possible to configure the CodePeople Light theme.
+*/
+
+require_once 'php/Mobile_Detect.php';
+
+add_action('admin_menu', 'codepeople_theme_switch_admin');
+
+// Initialize the admin panel 
+if (!function_exists("codepeople_theme_switch_admin")) { 
+	function codepeople_theme_switch_admin() { 
+		if (function_exists('add_options_page')) { 
+			add_options_page('Switch to CodePeople Light Theme', 'Switch to CodePeople Light Theme', 9, basename(__FILE__), 'codepeople_theme_switch_admin_page'); 
+		} 
+	}    
+}
+	
+// Print and process the admin page	
+if(!function_exists('codepeople_theme_switch_admin_page')){
+	function codepeople_theme_switch_admin_page(){
+		$image_dir = plugin_dir_url(__FILE__).'images';
+		$js_dir = plugin_dir_url(__FILE__).'js';
+
+		// Load the administration script
+		wp_enqueue_script('codepeople-theme-switch-admin-script', $js_dir.'/admin.js', array('jquery'));
+		wp_localize_script('codepeople-theme-switch-admin-script', 'config', array(
+			'image_path'  	=> $image_dir
+		));
+	
+		$codepeople_mobile_script_path   = (get_option('codepeople-mobile-general-theme')) ? get_option('codepeople-mobile-general-theme') : 'default';
+		$codepeople_mobile_style_path	 = (get_option('codepeople-mobile-general-theme')) ? get_option('codepeople-mobile-general-theme') : 'default';	
+		$codepeople_mobile_general_theme = (get_option('codepeople-mobile-general-theme')) ? get_option('codepeople-mobile-general-theme') : 'default';
+		$codepeople_mobile_header_theme  = (get_option('codepeople-mobile-header-theme'))  ? get_option('codepeople-mobile-header-theme')  : 'default';
+		$codepeople_mobile_footer_theme  = (get_option('codepeople-mobile-footer-theme'))  ? get_option('codepeople-mobile-footer-theme')  : 'default';
+		$codepeople_mobile_navbar_theme  = (get_option('codepeople-mobile-navbar-theme'))  ? get_option('codepeople-mobile-navbar-theme')  : 'default';
+		$codepeople_mobile_list_theme    = (get_option('codepeople-mobile-list-theme'))    ? get_option('codepeople-mobile-list-theme')    : 'default';
+				
+		if(isset($_POST['codepeople-theme-design-submit'])){
+				
+			echo '<div class="updated"><p><strong>'.__("Settings Updated").'</strong></div>';
+			
+			$codepeople_mobile_general_theme = $_POST['codepeople-mobile-general-theme'];
+			$codepeople_mobile_header_theme = $_POST['codepeople-mobile-header-theme'];
+			$codepeople_mobile_footer_theme = $_POST['codepeople-mobile-footer-theme'];
+			$codepeople_mobile_navbar_theme = $_POST['codepeople-mobile-navbar-theme'];
+			$codepeople_mobile_list_theme = $_POST['codepeople-mobile-list-theme'];
+			
+			
+			update_option('codepeople-mobile-general-theme', $codepeople_mobile_general_theme);
+			update_option('codepeople-mobile-header-theme', $codepeople_mobile_header_theme);
+			update_option('codepeople-mobile-footer-theme', $codepeople_mobile_footer_theme);
+			update_option('codepeople-mobile-navbar-theme', $codepeople_mobile_navbar_theme);
+			update_option('codepeople-mobile-list-theme', $codepeople_mobile_list_theme);
+			
+			
+		}
+	
+		echo '<div class="wrap">
+				<form method="post" action="'.$_SERVER['REQUEST_URI'].'">
+					<h2>CodePeople Light Theme Configuration</h2>
+					<table class="form-table" style="width:auto;">
+						<tbody>
+							<tr valign="top">
+								<th scope="row">
+									'.__('General theme design', 'codepeople-theme-switch-text').'</label>
+								</th>
+								<td colspan="2">
+									<table class="form-table">
+										<tr>
+											<td>
+												<select name="codepeople-mobile-general-theme" onchange="changeImage(this, \'#codepeople-mobile-general-theme\')">
+													'.codepeople_theme_switch_print_options($codepeople_mobile_general_theme, true).'
+												</select>
+											</td>
+										</tr>
+										<tr>		
+											<td>'.codepeople_theme_switch_print_image($codepeople_mobile_general_theme).'</td>
+										</tr>
+									</table>
+								</td>
+							</tr>
+							<tr valign="top">
+								<th scope="row">
+									'.__('Particular design for Headers', 'codepeople-theme-switch-text').'</label>
+								</th>
+								<td>
+									<select name="codepeople-mobile-header-theme" onchange="changeImage(this, \'#codepeople-mobile-header-theme\', \'element_\')">
+										'.codepeople_theme_switch_print_options($codepeople_mobile_header_theme).'
+									</select>
+								</td>
+								<td>'.codepeople_theme_switch_print_image($codepeople_mobile_header_theme, 'header').'</td>
+							</tr>
+							<tr valign="top">
+								<th scope="row">
+									'.__('Particular design for Footers', 'codepeople-theme-switch-text').'</label>
+								</th>
+								<td>
+									<select name="codepeople-mobile-footer-theme" onchange="changeImage(this, \'#codepeople-mobile-footer-theme\', \'element_\')">
+										'.codepeople_theme_switch_print_options($codepeople_mobile_footer_theme).'
+									</select>
+								</td>
+								<td>'.codepeople_theme_switch_print_image($codepeople_mobile_footer_theme, 'footer').'</td>
+							</tr>
+							<tr valign="top">
+								<th scope="row">
+									'.__('Particular design for Nav Bars', 'codepeople-theme-switch-text').'</label>
+								</th>
+								<td>
+									<select name="codepeople-mobile-navbar-theme" onchange="changeImage(this, \'#codepeople-mobile-navbar-theme\', \'element_\')">
+										'.codepeople_theme_switch_print_options($codepeople_mobile_navbar_theme).'
+									</select>
+								</td>
+								<td>'.codepeople_theme_switch_print_image($codepeople_mobile_navbar_theme, 'navbar').'</td>
+							</tr>
+							<tr valign="top">
+								<th scope="row">
+									'.__('Particular design for Lists', 'codepeople-theme-switch-text').'</label>
+								</th>
+								<td>
+									 <select name="codepeople-mobile-list-theme" onchange="changeImage(this, \'#codepeople-mobile-list-theme\', \'element_\')">
+										'.codepeople_theme_switch_print_options($codepeople_mobile_list_theme).'
+									</select>
+								</td>
+								<td>'.codepeople_theme_switch_print_image($codepeople_mobile_list_theme, 'list').'</td>
+							</tr>
+						</tbody>
+					</table>
+					<input type="hidden" name="codepeople-theme-design-submit" value="ok" />
+					<div class="submit"><input type="submit" class="button-primary" value="'.__('Update Settings', 'codepeople-theme-switch-text').'" /></div>
+				</form>
+			</div>';
+	}
+	
+	function codepeople_theme_switch_print_options($value, $general = false){
+		$codepeople_mobile_themes = array('default', 'a', 'b', 'c', 'd', 'e');
+		$result = '';
+		
+		foreach ($codepeople_mobile_themes as $theme){
+			$result .= '<option value="'.$theme.'" '.(($value == $theme) ? 'selected' : '').' >';
+			if($theme == 'default'){
+				$result .= ($general) ? __('Default theme', 'codepeople-theme-switch-text') : __('Inherited from general', 'codepeople-theme-switch-text');
+			}else{
+				$result .= __('Theme ', 'codepeople-theme-switch-text').strtoupper($theme);
+			}
+			$result .= '</option>';
+		}
+		
+		return $result;
+	} // codepeople_theme_switch_print_options
+
+	function codepeople_theme_switch_print_image($value, $prefix = ''){
+		$image_dir = plugin_dir_url(__FILE__).'images';
+		
+		if($value == 'default' && !empty($prefix)){
+			$result = '<img id="codepeople-mobile-'.$prefix.'-theme" src="'.$image_dir.'/empty.png">';
+		}else{
+			$result = '<img id="codepeople-mobile-'.((!empty($prefix)) ? $prefix : 'general').'-theme" src="'.$image_dir.'/'.((!empty($prefix)) ? 'element_' : '').$value.'.png">';
+		}
+		return $result;
+	} // codepeople_theme_switch_print_image
+}
+	
+	
+	
+add_filter('plugin_action_links_'.plugin_basename(__FILE__), 'codepeople_theme_switch_settings_link');
+
+if(!function_exists('codepeople_theme_switch_settings_link')){
+	function codepeople_theme_switch_settings_link($links){
+		$settings_link = '<a href="options-general.php?page=codepeople-theme-switch.php">'.__('Settings').'</a>'; 
+		array_unshift($links, $settings_link); 
+		return $links; 
+	}
+}
+
+/**
+ * Tell Wordpress to switch the theme based in the device where the webpage is loaded.
+ */
+add_filter('template', 'codepeople_mobile_switch_theme_by_device');
+add_filter('stylesheet', 'codepeople_mobile_switch_theme_by_device');
+
+if(!function_exists("codepeople_mobile_switch_theme_by_device")){
+	function codepeople_mobile_switch_theme_by_device($theme){
+		if (is_admin ())
+			return $theme;
+		
+		$detect = new Mobile_Detect();
+		if ($detect->isMobile() || $detect->isTablet()) {
+        	$theme = 'codepeople-mobile';
+    	}
+		
+		return $theme;	
+	}
+} // codepeople_mobile_switch_theme_by_device
+
+add_action('plugins_loaded', 'codepeople_theme_switch_init');
+if(!function_exists("codepeople_theme_switch_init")){
+	function codepeople_theme_switch_init(){
+		load_plugin_textdomain('codepeople-theme-switch-text', false, dirname(plugin_basename(__FILE__)) . '/../languages/');
+	}
+} // codepeople_theme_switch_init
+?>
