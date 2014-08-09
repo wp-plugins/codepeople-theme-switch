316f88b9734aecddb53b1785328f548a3b625fd2
codepeople <codepeople@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-09 23:27:08 +0000

    The current update improves the way that the plugin detects the devices width.
    git-svn-id: https://plugins.svn.wordpress.org/codepeople-theme-switch/trunk@963147 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/public.js
+++ js/public.js
@@ -2,9 +2,26 @@ jQuery( function ($){
 
 	if( typeof codepeople_theme_switch != 'undefined' )
 	{
-		var width = $( window ).width();
-
-		if( width < codepeople_theme_switch[ 'width' ] )
+        function getWidth()
+        {
+            var myWidth = 0;
+            if( typeof( window.innerWidth ) == 'number' ) {
+                //Non-IE
+                myWidth = window.innerWidth;
+            } else if( document.documentElement && document.documentElement.clientWidth ) {
+                //IE 6+ in 'standards compliant mode'
+                myWidth = document.documentElement.clientWidth;
+            } else if( document.body && document.body.clientWidth ) {
+                //IE 4 compatible
+                myWidth = document.body.clientWidth;
+            }
+            
+            return ( typeof screen != 'undefined' ) ? Math.min( screen.width, myWidth ) : myWidth;
+        };
+        
+		var width = getWidth();
+		
+        if( width < codepeople_theme_switch[ 'width' ] )
 		{
 			var selection = window.confirm( codepeople_theme_switch[ 'message' ] );
 			if( selection )
